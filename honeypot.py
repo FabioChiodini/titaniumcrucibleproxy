332d37f4bbda451348e4421e881aa495b0d1f71e
Fabio Chiodini <FabioChiodini@users.noreply.github.com>
2018-02-08 10:21:00 +0100

    Update honeypot.py

--- honeypot.py
+++ honeypot.py
@@ -5,6 +5,42 @@ import logging
 import logstash
 import sys
 
+ def get_remote_addr(self, forwarded_for):
+        """Selects the new remote addr from the given list of ips in
+        X-Forwarded-For.  By default the last one is picked. Specify
+        num_proxy_servers=2 to pick the second to last one, and so on.
+        """
+        if self.detect_misconfiguration and not forwarded_for:
+            raise Exception("SaferProxyFix did not detect a proxy server. Do not use this fixer if you are not behind a proxy.")
+        if self.detect_misconfiguration and len(forwarded_for) < self.num_proxy_servers:
+            raise Exception("SaferProxyFix did not detect enough proxy servers. Check your num_proxy_servers setting.")
+
+        if forwarded_for and len(forwarded_for) >= self.num_proxy_servers:
+            return forwarded_for[-1 * self.num_proxy_servers]
+
+    def __call__(self, environ, start_response):
+        getter = environ.get
+        forwarded_proto = getter('HTTP_X_FORWARDED_PROTO', '')
+        forwarded_for = getter('HTTP_X_FORWARDED_FOR', '').split(',')
+        forwarded_host = getter('HTTP_X_FORWARDED_HOST', '')
+        environ.update({
+            'werkzeug.proxy_fix.orig_wsgi_url_scheme':  getter('wsgi.url_scheme'),
+            'werkzeug.proxy_fix.orig_remote_addr':      getter('REMOTE_ADDR'),
+            'werkzeug.proxy_fix.orig_http_host':        getter('HTTP_HOST')
+        })
+        forwarded_for = [x for x in [x.strip() for x in forwarded_for] if x]
+        remote_addr = self.get_remote_addr(forwarded_for)
+        if remote_addr is not None:
+            environ['REMOTE_ADDR'] = remote_addr
+        if forwarded_host:
+            environ['HTTP_HOST'] = forwarded_host
+        if forwarded_proto:
+            environ['wsgi.url_scheme'] = forwarded_proto
+        return self.app(environ, start_response)
+
+from saferproxyfix import SaferProxyFix
+app.wsgi_app = SaferProxyFix(app.wsgi_app)
+
 #if 'LOG_HOST' not in os.environ:
 #    raise(Exception("LOG_HOST NOT DEFINED"))
 
@@ -30,7 +66,7 @@ def log_request(req):
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 
-app.wsgi_app = ProxyFix(app.wsgi_app)
+#app.wsgi_app = ProxyFix(app.wsgi_app)
 
 def honey(path):
     log_request(request)
